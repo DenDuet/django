ef79203a0cd9575e87dbf3afdfc4fd305891757c
Denis <9581886@gmail.com>
2023-10-02 01:21:48 +0300

    dz2

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'dz1',
+    # 'dz1',
+    'dz2',
 ]
 
 MIDDLEWARE = [
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('dz1.urls')),
+    # path('', include('dz1.urls')),
+    path('', include('dz2.urls')),    
 ]
